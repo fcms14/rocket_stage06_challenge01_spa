14ab925acab17425522b3d71da3c8c76fcf9ba5e
fcms14 <felipecms14@hotmail.com>
2022-05-12 10:33:53 -0300

    jsLoadImg

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ import { Router } from './router.js';
 
 const router = new Router();
 
-router.add(404,'/pages/404.html')
-router.add('/exploracao','/pages/exploracao.html')
-router.add('/index.html','/pages/home.html')
-router.add('/universo','/pages/universo.html')
-router.add('/','/pages/home.html')
+router.add(404,'/pages/404.html','assets/404.jpg')
+router.add('/exploracao','/pages/exploracao.html', 'assets/mountains-universe-3.png')
+router.add('/index.html','/pages/home.html', 'assets/mountains-universe-1.png')
+router.add('/universo','/pages/universo.html', 'assets/mountains-universe-2.png')
+router.add('/','/pages/home.html', 'assets/mountains-universe-1.png')
 
 router.handle();
 
-window.onpopstate = async () => await router.handle();
+window.onpopstate = () => router.handle();
 window.route = () => router.route();
